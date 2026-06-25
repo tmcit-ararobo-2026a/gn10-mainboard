874bc04a3c1aeb5a1a75b391fd9aaca0598dddc1
Kamagata0 <aroe0725@gmail.com>
2026-06-25 16:30:19 +0900

    chore:desk_fingerの値に200をかけた

--- gn10_mainboard/src/app.cpp
+++ gn10_mainboard/src/app.cpp
@@ -169,7 +169,7 @@ void loop()
     float desk_arm_velocities[4];
     arm_velocities[0] = operation.desk_depth * 200.0f;
     arm_velocities[1] = operation.desk_lift * 200.0f;
-    arm_velocities[2] = operation.desk_finger;
+    arm_velocities[2] = operation.desk_finger * 200.0f;
     arm_velocities[3] = 0.0f;
     desk_arm.set_angular_velocities(desk_arm_velocities);
     // Basic System Process
