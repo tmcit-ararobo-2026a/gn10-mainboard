1cf85394bd27255c81f5a670f6da856d3e114b71
kanai0914 <ayuayugonbe@gmail.com>
2026-08-12 14:26:17 +0900

    feat:何度も初期化処理を送信できるプログラムとデバッグ用プログラム実装

--- gn10_mainboard/src/app.cpp
+++ gn10_mainboard/src/app.cpp
@@ -143,7 +143,7 @@ void loop()
     }
 
     // Control the belt-type injection
-    if (operation.belt_init && !initilized_belt) {
+    if (operation.belt_init) {
         initilized_belt = true;
         vesc_hub.set_init(0, motor_config_belt);
     }
@@ -159,7 +159,11 @@ void loop()
 
     // Get latest belt angular velocity
     if (vesc_hub.get_angular_velocity_feedbacks(vesc_velocities_feedbacks)) {
-        serial_printf("%f\n", vesc_velocities_feedbacks[0]);
+        serial_printf("1:%f\n", vesc_velocities_feedbacks[0]);
+        serial_printf("2:%f\n", vesc_velocities_feedbacks[1]);
+        serial_printf("3:%f\n", vesc_velocities_feedbacks[2]);
+        serial_printf("4:%f\n", vesc_velocities_feedbacks[3]);
+        serial_printf("\n");
     }
 
     // Control the air-type injection
