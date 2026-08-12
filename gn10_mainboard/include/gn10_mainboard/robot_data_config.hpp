98c6b6b98aadaa785780f12f64b137b6561c665e
aiba-gento <8gn24gn25@gmail.com>
2026-08-12 23:53:00 +0900

    feat:ロボット共通データや通信定義の修正および再構築

--- gn10_mainboard/include/gn10_mainboard/robot_data_config.hpp
+++ gn10_mainboard/include/gn10_mainboard/robot_data_config.hpp
@@ -8,8 +8,8 @@
  * @copyright Copyright (c) 2025
  *
  * socket_cmd (port:26574)
- *  |-  operation_data    pc          ->  main-board
- *  |-  feedback_data     main-board  ->  pc
+ *  |-  operation    pc          ->  main-board
+ *  |-  feedback     main-board  ->  pc
  *
  * socket_teleop (port:10410)
  *  |-  teleop            robo-con    ->  pc ( main (in debug_mode))
@@ -21,14 +21,14 @@
 #pragma once
 #include <stdint.h>
 
-namespace robot_network_config {
+namespace robot_config {
 
 namespace header {
-constexpr uint16_t operation_data = 0xAB36;
-constexpr uint16_t feedback_data  = 0x554A;
-constexpr uint8_t teleop_data     = 0xAA;
-constexpr uint16_t pc_debug       = 0x38F7;
-constexpr uint16_t main_debug     = 0x2A84;
+constexpr uint8_t operation  = 0xAB;
+constexpr uint8_t feedback   = 0x55;
+constexpr uint8_t teleop     = 0xAA;
+constexpr uint8_t pc_debug   = 0x38;
+constexpr uint8_t main_debug = 0x2A;
 }  // namespace header
 
 namespace port {
@@ -38,54 +38,80 @@ constexpr uint16_t debug  = 39244;
 }  // namespace port
 
 namespace ip {
-constexpr uint8_t mainboard[] = {192, 168, 3, 2};
-constexpr uint8_t pc_robot[]  = {192, 168, 3, 1};
-constexpr uint8_t pc_wifi[]   = {192, 168, 2, 1};
-constexpr uint8_t teleop[]    = {192, 168, 2, 2};
+uint8_t mainboard[] = {192, 168, 3, 2};
+uint8_t pc_robot[]  = {192, 168, 3, 1};
+uint8_t pc_wifi[]   = {192, 168, 2, 1};
+uint8_t teleop[]    = {192, 168, 2, 2};
 }  // namespace ip
+
+/**
+ * @brief ロボットの動作司令値 32byte
+ *
+ */
+struct operation_t {
+    // 識別ヘッダー 1byte
+    uint8_t header;
+    // 足回り 12byte
+    float x_vel;        //[m/s]
+    float y_vel;        //[m/s]
+    float angular_vel;  //[rad/s]
+    // バケツ用アーム 2byte
+    uint8_t bucket_arm_hight;  //[cm]
+    uint8_t bucket_arm_hold;
+    // 装填機構 2byte
+    uint8_t loading_hook_pos;  //[phase]
+    bool loading_shift_cloth;
+    // ベルト直動 6byte
+    float belt_vel;  //[m/s]
+    bool belt_throw;
+    bool belt_init;
+    // エアシリンダー射出 3byte
+    bool air_rauncher_for_flag;
+    bool air_rauncher_for_desk_r;
+    bool air_rauncher_for_desk_l;
+    // 机上雑巾回収 2byte
+    uint8_t desk_arm_pos;  //[cm]
+    bool desk_arm_tip_angle;
+    // 状態表示 2byte
+    int8_t target_bucket_angle_roll;
+    int8_t target_bucket_angle_pitch;
+    // 予備 2byte
+    uint8_t reserved[2];
+} __attribute__((__packed__));
+
+union operation_u {
+    robot_config::operation_t value;                    // 操作データ
+    uint8_t binary[sizeof(robot_config::operation_t)];  // 送信バイト配列
+} __attribute__((__packed__));
+
+static_assert(sizeof(operation_t) == 32);
 
 /**
  * @brief ロボットのセンサ値などのフィードバック
  *
  */
-struct feedback_data_t {
-    uint16_t header;  // ヘッダー
+struct feedback_t {
+    uint8_t header;  // ヘッダー
 } __attribute__((__packed__));
 
-/**
- * @brief ロボットの動作司令値
- *
- */
-struct operation_data_t {
-    uint16_t header;  // ヘッダー
-    float wheel_front;
-    float wheel_back_left;
-    float wheel_back_right;
-    float belt_velocity;
-    float arm_horizontal;
-    float arm_vertical;
-    float desk_lift;
-    float desk_depth;
-    float desk_finger;
-    bool arm_hold;
-    bool belt_throw;
-    bool collect;
-    bool air_throw;
-    bool belt_init;
-    uint8_t reserved[21];
+union feedback_u {
+    robot_config::feedback_t value;
+    uint8_t binary[sizeof(robot_config::feedback_t)];
 } __attribute__((__packed__));
+
+static_assert(sizeof(feedback_t) == 1);
 
 /**
  * @brief 操縦デバイスのレバーの傾きと押し込み
  *
  */
-enum lever_point_t {
-    front,
-    right,
-    right_deep,
-    left,
-    left_deep,
-    push,
+enum class LeverPosition : uint8_t {
+    FRONT,
+    RIGHT,
+    RIGHT_DEEP,
+    LEFT,
+    LEFT_DEEP,
+    PUSH,
 };
 
 /**
@@ -94,62 +120,66 @@ enum lever_point_t {
  */
 struct teleop_t {
     uint8_t header;  // 認識番号
-    struct {
-        struct {
-            int8_t x;
-            int8_t y;
-            uint8_t push : 1;
-        } __attribute__((__packed__)) right;
-        struct {
-            int8_t x;
-            int8_t y;
-            uint8_t push : 1;
-        } __attribute__((__packed__)) left;
-    } __attribute__((__packed__)) stick;  // 34 bit = 4Byte + 2bit
 
     struct {
-        lever_point_t right : 3;
-        lever_point_t left  : 3;
-    } __attribute__((__packed__)) lever;  // 6 bit
+        int8_t stick_right[2];             // 0:x, 1:y
+        int8_t stick_left[2];              // 0:x, 1:y
+    } __attribute__((__packed__)) analog;  // 4byte
 
     struct {
-        struct {
-            uint8_t button_5 : 1;
-            uint8_t button_6 : 1;
-            uint8_t button_7 : 1;
-        } __attribute__((__packed__)) right;
-        struct {
-            uint8_t button_1 : 1;
-            uint8_t button_2 : 1;
-            uint8_t button_3 : 1;
-            uint8_t button_4 : 1;
-        } __attribute__((__packed__)) left;
-        uint8_t reserved : 1;
-    } __attribute__((__packed__)) button;  // 1Byte
+        LeverPosition lever_right : 3;
+        LeverPosition lever_left  : 3;
+        uint8_t stick_push_right  : 1;
+        uint8_t stick_push_left   : 1;
+        uint8_t up                : 1;
+        uint8_t down              : 1;
+        uint8_t right             : 1;
+        uint8_t left              : 1;
+        uint8_t circle            : 1;
+        uint8_t cross             : 1;
+        uint8_t triangle          : 1;
+        uint8_t reserved          : 1;
+    } __attribute__((__packed__)) buttons;  // 2byte
 
-    uint8_t data_CRC;  // 1Byte
-    /***
-     * CRC以外を除いた6Byteの和の補数
+    /**
+     * checksum以外を除いた7Byteの和の補数
      * ただし計算結果の8bitより大きい値は切り捨て
      */
-
+    uint8_t data_checksum;
 } __attribute__((__packed__));
+
+union teleop_u {
+    robot_config::teleop_t value;
+    uint8_t binary[sizeof(robot_config::teleop_t)];
+} __attribute__((__packed__));
+
+static_assert(sizeof(teleop_t) == 8);
 
 /**
  * @brief PCのデバッグ用通信（再起動やスクリプト開始など）
  *
  */
-struct pc_debug_t {
-    uint16_t header;  // ヘッダー
+struct debug_pc_t {
+    uint8_t header;  // ヘッダー
     bool jetson_restart;
+} __attribute__((__packed__));
+
+union debug_pc_u {
+    robot_config::debug_pc_t value;
+    uint8_t binary[sizeof(robot_config::debug_pc_t)];
 } __attribute__((__packed__));
 
 /**
  * @brief メイン基板(RobotControlHub)のデバッグ用通信（制御モード切り替えなど）
  *
  */
-struct main_debug_t {
-    uint16_t header;
+struct debug_main_t {
+    uint8_t header;
 } __attribute__((__packed__));
 
-}  // namespace robot_network_config
+union debug_main_u {
+    robot_config::debug_main_t value;
+    uint8_t binary[sizeof(robot_config::debug_main_t)];
+} __attribute__((__packed__));
+
+}  // namespace robot_config
