6060266805e1aa5cb3f651752e1c26e93ec96faa
aiba-gento <8gn24gn25@gmail.com>
2026-08-13 00:38:29 +0900

    feat:角度は変更できないので、曲げるという表現に修正

--- gn10_mainboard/include/gn10_mainboard/robot_config.hpp
+++ gn10_mainboard/include/gn10_mainboard/robot_config.hpp
@@ -71,7 +71,7 @@ struct operation_t {
     bool air_rauncher_for_desk_l;
     // 机上雑巾回収 2byte
     uint8_t desk_arm_pos;  //[cm]
-    bool desk_arm_tip_angle;
+    bool desk_arm_hold;
     // 状態表示 2byte
     int8_t target_bucket_angle_roll;
     int8_t target_bucket_angle_pitch;
