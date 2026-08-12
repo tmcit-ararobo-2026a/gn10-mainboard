b3e464cb99dd7d965e430be563598298869ed971
Maidui3 <lifelong1027@gmail.com>
2026-08-12 09:51:33 +0900

    feat:コントローラー入力の構造体を追加し、レバーとボタンの定義を拡張

--- gn10_mainboard/include/gn10_mainboard/robot_data_config.hpp
+++ gn10_mainboard/include/gn10_mainboard/robot_data_config.hpp
@@ -38,3 +38,58 @@ struct operation_data_t {
     bool belt_init;
     uint8_t reserved[21];
 } __attribute__((__packed__));
+
+enum lever_point_t {
+    front,
+    right,
+    right_deep,
+    left,
+    left_deep,
+    push,
+};
+
+union controller_input_u {
+    struct {
+        struct {
+            struct {
+                uint8_t x;
+                uint8_t y;
+                bool push;
+            } right;
+            struct {
+                uint8_t x;
+                uint8_t y;
+                bool push;
+            } left;
+        } stick;  // 34 bit = 4Byte + 2bit
+
+        struct {
+            lever_point_t right : 3;
+            lever_point_t left  : 3;
+        } lever;  // 6 bit
+
+        struct {
+            struct {
+                bool button_5;
+                bool button_6;
+                bool button_7;
+            } right;
+            struct {
+                bool button_1;
+                bool button_2;
+                bool button_3;
+                bool button_4;
+            } left;
+            bool received;
+        } button;  // 1Byte
+
+        uint8_t data_CRC;  // 1Byte
+        /***
+         * CRC以外を除いた6Byteの和の補数
+         * ただし計算結果の8bitより大きい値は切り捨て
+         */
+
+    } __attribute__((__packed__));
+
+    uint8_t data[7];
+};
