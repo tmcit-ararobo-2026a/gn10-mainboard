f139f67fd7a13e984054459100199cb4ac21477c
kanai0914 <ayuayugonbe@gmail.com>
2026-08-14 17:44:43 +0900

    feat:setter

--- gn10_mainboard/include/gn10_mainboard/conversion_command.hpp
+++ gn10_mainboard/include/gn10_mainboard/conversion_command.hpp
@@ -3,11 +3,33 @@
 class ConversionCommand
 {
 public:
-    robot_config::command_t conversion(robot_config::teleop_t teleop);
+    ConversionCommand(
+        uint8_t bucket_limit_h, uint8_t bucket_limit_l, uint8_t desk_limit_h, uint8_t desk_limit_l
+    );
+
+    void set_belt_vel(const uint8_t belt_vel);
+
+    void set_bucket_distance_value(const uint8_t bucket_distance);
+
+    void set_desk_distance_value(const uint8_t desk_distance);
+
+    robot_config::command_t conversion(robot_config::teleop_t& teleop);
 
 private:
-    robot_config::teleop_t teleop_;
+    robot_config::teleop_t teleop_last_;
     robot_config::command_t command_;
-    uint8_t count    = 1;  // エアシリンダー射出先: 0=desk_l, 1=flag, 2=desk_r
-    bool circle_last = false;
+
+    // エアシリンダー射出先: 0=desk_l, 1=flag, 2=desk_r
+    uint8_t count = 1;
+
+    // constructor
+    uint8_t bucket_limit_h_;
+    uint8_t bucket_limit_l_;
+    uint8_t desk_limit_h_;
+    uint8_t desk_limit_l_;
+
+    // seter
+    uint8_t belt_vel_;
+    uint8_t bucket_distance_;
+    uint8_t desk_distance_;
 };
--- gn10_mainboard/src/conversion_command.cpp
+++ gn10_mainboard/src/conversion_command.cpp
@@ -1,45 +1,67 @@
 #include "gn10_mainboard/conversion_command.hpp"
 
-robot_config::command_t ConversionCommand::conversion(robot_config::teleop_t teleop)
+ConversionCommand::ConversionCommand(
+    uint8_t bucket_limit_h, uint8_t bucket_limit_l, uint8_t desk_limit_h, uint8_t desk_limit_l
+)
+    : bucket_limit_h_(bucket_limit_h),
+      bucket_limit_l_(bucket_limit_l),
+      desk_limit_h_(desk_limit_h),
+      desk_limit_l_(desk_limit_l)
 {
-    teleop_ = teleop;
+}
 
+void ConversionCommand::set_belt_vel(const uint8_t belt_vel)
+{
+    belt_vel_ = belt_vel;
+}
+
+void ConversionCommand::set_bucket_distance_value(const uint8_t bucket_distance)
+{
+    bucket_distance_ = bucket_distance;
+}
+
+void ConversionCommand::set_desk_distance_value(const uint8_t desk_distance)
+{
+    desk_distance_ = desk_distance;
+}
+
+robot_config::command_t ConversionCommand::conversion(robot_config::teleop_t& teleop)
+{
     // 足回り
-    command_.x_vel       = teleop_.analog.stick_left[0];
-    command_.y_vel       = teleop_.analog.stick_left[1];
-    command_.angular_vel = teleop_.analog.stick_right[0];
+    command_.x_vel       = teleop.analog.stick_left[0];
+    command_.y_vel       = teleop.analog.stick_left[1];
+    command_.angular_vel = teleop.analog.stick_right[0];
 
     /*バケツ回収 limitは受信側でかけること！*/
-    if (teleop_.buttons.up) {
+    if (teleop.buttons.up) {
         command_.bucket_arm_hight += 10;
-    } else if (teleop_.buttons.down) {
+    } else if (teleop.buttons.down) {
         command_.bucket_arm_hight -= 10;
     }
-    if (teleop_.buttons.right) {
+    if (teleop.buttons.right) {
         command_.bucket_arm_hold += 10;
-    } else if (teleop_.buttons.left) {
+    } else if (teleop.buttons.left) {
         command_.bucket_arm_hold -= 10;
     }
 
     /* 机上回収  limitは受信側でかけること!*/
     // 引き入れ
-    if (teleop_.buttons.triangle) {
+    if (teleop.buttons.triangle) {
         command_.desk_arm_pos += 10;
-    } else if (teleop_.buttons.cross) {
+    } else if (teleop.buttons.cross) {
         command_.desk_arm_pos -= 10;
     }
 
     // アーム曲げ
-    if (teleop_.buttons.circle && !circle_last) {
+    if (teleop.buttons.circle && teleop_last_.buttons.circle) {
         command_.desk_arm_hold = !command_.desk_arm_hold;
     }
-    circle_last = teleop_.buttons.circle;
 
     /*装填機構*/
-    command_.loading_shift_cloth = teleop_.buttons.cross;
+    command_.loading_shift_cloth = teleop.buttons.cross;
 
     /*ベルト直動*/
-    switch (teleop_.buttons.lever_right) {
+    switch (teleop.buttons.lever_right) {
         case robot_config::LeverPosition::FRONT:
             break;
 
@@ -72,14 +94,14 @@ robot_config::command_t ConversionCommand::conversion(robot_config::teleop_t tel
     }
 
     // init処理
-    if (teleop_.buttons.stick_push_left || teleop_.buttons.stick_push_right) {
+    if (teleop.buttons.stick_push_left && teleop.buttons.stick_push_right) {
         command_.belt_init = true;
     } else {
         command_.belt_init = false;
     }
 
     // エアシリンダー
-    switch (teleop_.buttons.lever_left) {
+    switch (teleop.buttons.lever_left) {
         case robot_config::LeverPosition::RIGHT:
         case robot_config::LeverPosition::RIGHT_DEEP:
             if (count == 2) {
@@ -118,5 +140,6 @@ robot_config::command_t ConversionCommand::conversion(robot_config::teleop_t tel
             break;
     }
 
+    teleop_last_ = teleop;
     return command_;
 }
