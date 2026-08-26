ac2106607d8740b76dd4d737943719fc5ccfa4a5
kanai0914 <ayuayugonbe@gmail.com>
2026-08-26 11:59:47 +0900

    feat:gn10の設定など反映loadingプログラム追加（未完）

--- gn10_mainboard/src/app.cpp
+++ gn10_mainboard/src/app.cpp
@@ -26,7 +26,9 @@ constexpr uint32_t k_heartbeat_toggle_interval_ms = 500;
 
 uint32_t heartbeat_last_toggle_time_ms = 0;
 // Retained Data
-float vesc_velocities_feedbacks[4];
+float vesc_feedbacks[4];
+
+float loading_feedbacks[4];
 
 /**
  * @brief Toggle heartbeat LED at a fixed interval.
@@ -45,6 +47,7 @@ gn10_can::devices::power_manager::Config power_manager_config;
 gn10_can::devices::MotorConfig motor_config_wheel;
 gn10_can::devices::MotorConfig motor_config_arm;
 gn10_can::devices::MotorConfig motor_config_belt;
+gn10_can::devices::MotorConfig motor_config_loading;
 // CAN Drivers
 gn10_can::drivers::CANDriver can1_driver(&hfdcan1);
 gn10_can::drivers::FDCANDriver fdcan2_driver(&hfdcan2);
@@ -62,6 +65,7 @@ gn10_can::devices::PowerManagerClient power_manager(fdcan2_bus, 0);
 gn10_can::devices::ESCHubClient vesc_hub(fdcan3_bus, 0);
 gn10_can::devices::ESCHubClient esc_wheel(fdcan3_bus, 1);
 gn10_can::devices::ESCHubClient esc_arm(fdcan3_bus, 2);
+gn10_can::devices::ESCHubClient esc_loading(fdcan3_bus, 2);
 
 // Ethernet
 RobotEthernet ether;
@@ -69,6 +73,11 @@ robot_config::debug_pc_t prev_debug_pc = {};
 // belt
 bool initilized_belt = false;
 float vesc_vel       = 0.0f;
+
+// loading
+bool start_encoder      = false;
+bool last_start_encoder = false;
+bool loading            = true;
 // Inverse Kinematics
 ThreeWheelOmni omni(0.5f, 0.1f);
 constexpr float M3508_GEAR_RATIO = 19.0f;
@@ -81,12 +90,12 @@ void command_robot_drivers(const robot_config::command_t& command)
     omni.convert(command.x_vel, command.y_vel, command.angular_vel, 0.0f);
     float front, right, left;
     omni.getWheelAngularVelocity(&front, &left, &right);
-    float wheel_angular_velocites[4];
-    wheel_angular_velocites[0] = front * M3508_GEAR_RATIO;
-    wheel_angular_velocites[1] = left * M3508_GEAR_RATIO;
-    wheel_angular_velocites[2] = right * M3508_GEAR_RATIO;
-    wheel_angular_velocites[3] = 0.0f;
-    esc_wheel.set_angular_velocities(wheel_angular_velocites);
+    float wheel_target[4];
+    wheel_target[0] = front * M3508_GEAR_RATIO;
+    wheel_target[1] = left * M3508_GEAR_RATIO;
+    wheel_target[2] = right * M3508_GEAR_RATIO;
+    wheel_target[3] = 0.0f;
+    esc_wheel.set_targets(wheel_target);
 
     // Control the belt-type injection
     if (command.belt_init) {
@@ -100,8 +109,8 @@ void command_robot_drivers(const robot_config::command_t& command)
         vesc_vel = 0.0f;
     }
 
-    float vesc_velocities[4] = {vesc_vel, 0.0f, 0.0f, 0.0f};
-    vesc_hub.set_angular_velocities(vesc_velocities);
+    float vesc_target[4] = {vesc_vel, 0.0f, 0.0f, 0.0f};
+    vesc_hub.set_targets(vesc_target);
     // Control the air-type injection
     std::array<bool, 8> targets{};
     targets[0] = command.air_rauncher_for_flag;
@@ -110,24 +119,31 @@ void command_robot_drivers(const robot_config::command_t& command)
     solenoid.set_target(targets);
 
     // Control the arm with C610
-    float arm_velocities[4];
-    arm_velocities[0] = command.bucket_arm_hight;
-    arm_velocities[1] = (float)command.bucket_arm_hold;  // bool
-    esc_arm.set_angular_velocities(arm_velocities);
+    float arm_and_loading_target[4];
+    arm_and_loading_target[0] = command.bucket_arm_hight;
+    arm_and_loading_target[1] = (float)command.bucket_arm_hold;  // bool
+
+    // Control the loadinf with C610
+    if (start_encoder && loading) {
+        arm_and_loading_target[4] = 120.0f;
+        loading                   = false;
+    }
+
+    esc_arm.set_targets(arm_and_loading_target);
 
     serial_printf(
         "f:%3.1f, l:%3.1f, r:%3.1f, vesc:%.2f, air:%d, %d, %d, arm:%.2f, %.2f, %.2f, %.2f\n",
-        wheel_angular_velocites[0],
-        wheel_angular_velocites[1],
-        wheel_angular_velocites[2],
-        vesc_velocities[0],
+        wheel_target[0],
+        wheel_target[1],
+        wheel_target[2],
+        vesc_target[0],
         targets[0],
         targets[1],
         targets[2],
-        arm_velocities[0],
-        arm_velocities[1],
-        arm_velocities[2],
-        arm_velocities[3]
+        arm_and_loading_target[0],
+        arm_and_loading_target[1],
+        arm_and_loading_target[2],
+        arm_and_loading_target[3]
     );
 }
 }  // namespace
@@ -145,8 +161,10 @@ void setup()
     // Motor configuration
     motor_config_wheel.set_max_duty_ratio(0.5f);
     motor_config_wheel.set_motor_type(gn10_can::devices::MotorType::C620);
+    motor_config_wheel.set_encoder_type(gn10_can::devices::EncoderType::IncrementalSpeed);
     motor_config_arm.set_max_duty_ratio(0.5f);
     motor_config_arm.set_motor_type(gn10_can::devices::MotorType::C610);
+    motor_config_arm.set_encoder_type(gn10_can::devices::EncoderType::IncrementalSpeed);
 
     motor_config_belt.set_motor_type(gn10_can::devices::MotorType::VESC);
 
@@ -157,10 +175,14 @@ void setup()
     for (uint8_t i = 0; i < 4; i++) {
         esc_wheel.set_init(i, motor_config_wheel);
         esc_wheel.set_gains(i, 0.05f, 0.0f, 0.0f, 0.0f);
+    }
+    for (uint8_t i = 0; i < 3; i++) {
         esc_arm.set_init(i, motor_config_arm);
         esc_arm.set_gains(i, 0.05f, 0.0f, 0.0f, 0.0f);
     }
 
+    esc_loading.set_init(4, motor_config_loading);
+    esc_loading.set_gains(4, 0.05f, 0.0f, 0.0f, 0.0f);
     // Initialize Ethernet
     ether.init();
 
@@ -191,13 +213,17 @@ void loop()
         command_robot_drivers(command);
     }
     // Get latest belt angular velocity
-    if (vesc_hub.get_angular_velocity_feedbacks(vesc_velocities_feedbacks)) {
-        serial_printf("1:%f\n", vesc_velocities_feedbacks[0]);
-        serial_printf("2:%f\n", vesc_velocities_feedbacks[1]);
-        serial_printf("3:%f\n", vesc_velocities_feedbacks[2]);
-        serial_printf("4:%f\n", vesc_velocities_feedbacks[3]);
-        serial_printf("\n");
+    if (vesc_hub.get_feedbacks(vesc_feedbacks)) {
+        start_encoder = true;
+        loading       = true;
+        serial_printf("1:%f\n", vesc_feedbacks[0]);
     }
+
+    if (start_encoder && !last_start_encoder) {
+        motor_config_loading.set_encoder_type(gn10_can::devices::EncoderType::IncrementalTotal);
+        last_start_encoder = start_encoder;
+    }
+
     // ボタンが押されたら送る処理
     robot_config::debug_pc_t current_debug_pc = {};
     current_debug_pc.jetson_restart =
