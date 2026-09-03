48d92857eb080fa3ffbe0b8c15d391e8d72b3c1b
aiba-gento <8gn24gn25@gmail.com>
2026-09-03 09:00:21 +0900

    feat:CANの受信を3回まで連続でできるようにした

--- gn10_mainboard/src/app.cpp
+++ gn10_mainboard/src/app.cpp
@@ -315,27 +315,52 @@ void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs)
 {
     (void)RxFifo0ITs;
     if (hfdcan->Instance == hfdcan1.Instance) {
-        can1_bus.update();
+        uint8_t timeout_counter = 0;
+        while (HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO0) > 0 && timeout_counter < 3) {
+            can1_bus.update();
+            timeout_counter++;
+        }
+
     } else if (hfdcan->Instance == hfdcan2.Instance) {
-        fdcan2_bus.update();
+        uint8_t timeout_counter = 0;
+        while (HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO0) > 0 && timeout_counter < 3) {
+            fdcan2_bus.update();
+            timeout_counter++;
+        }
     } else if (hfdcan->Instance == hfdcan3.Instance) {
-        fdcan3_bus.update();
+        uint8_t timeout_counter = 0;
+        while (HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO0) > 0 && timeout_counter < 3) {
+            fdcan3_bus.update();
+            timeout_counter++;
+        }
     }
 }
 
 /**
  * @brief Receive callback for FDCAN FIFO1.
  */
-void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs)
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo1ITs)
 {
+    (void)RxFifo1ITs;
     if (hfdcan->Instance == hfdcan1.Instance) {
-        can1_bus.update();
+        uint8_t timeout_counter = 0;
+        while (HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO1) > 0 && timeout_counter < 3) {
+            can1_bus.update();
+            timeout_counter++;
+        }
 
     } else if (hfdcan->Instance == hfdcan2.Instance) {
-        fdcan2_bus.update();
-
+        uint8_t timeout_counter = 0;
+        while (HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO1) > 0 && timeout_counter < 3) {
+            fdcan2_bus.update();
+            timeout_counter++;
+        }
     } else if (hfdcan->Instance == hfdcan3.Instance) {
-        fdcan3_bus.update();
+        uint8_t timeout_counter = 0;
+        while (HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO1) > 0 && timeout_counter < 3) {
+            fdcan3_bus.update();
+            timeout_counter++;
+        }
     }
 }
 }
