c6ffc1d80d7b86acb660902131d53e29c341e1ec
aiba-gento <8gn24gn25@gmail.com>
2026-09-10 15:30:37 +0900

    feat:ハンドの力調整

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -44,7 +44,7 @@ constexpr float RELOAD_PID_GAINS[3] = {-1.5f, 0.0f, 0.0f};
 constexpr float BUCKET_ARM_HEIGHT_PULLEY_RADIUS = 0.04f;   // [m]
 constexpr float BUCKET_ARM_HEIGHT_MAX           = 0.6f;    // [m]
 constexpr float BUCKET_ARM_HEIGHT_MIN           = 0.075f;  // [m]
-constexpr float BUCKET_ARM_HOLD_FORCE           = 1.0f;    // [A]
+constexpr float BUCKET_ARM_HOLD_FORCE           = 2.4f;    // [A]
 constexpr float BUCKET_ARM_RELEASE_FORCE        = 1.0f;    // [A]
 // 機械定数
 constexpr float M3508_GEAR_RATIO = 19.0f;
